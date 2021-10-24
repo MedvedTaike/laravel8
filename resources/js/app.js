a20df3a0d48316b86b61e7815cc245bdcb983b54
Medved Taike <taike_dev@mail.ru>
2021-10-24 14:43:07 +0600

    Set environment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-require('./bootstrap');
+import Vue from 'vue' 
+import App from './App.vue'
+// import router from './router'
+import VueResource from 'vue-resource';
+import Vuelidate from 'vuelidate'
+
+Vue.use(Vuelidate)
+Vue.use(VueResource)
+Vue.http.options.credentials = true;
+Vue.http.interceptors.push(function(request) {
+  request.headers.set('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
+  request.headers.set('Accept', 'application/json');
+  request.headers.set('Content-Type', 'application/json');
+});
+
+new Vue({
+    // router,
+    render: h => h(App)
+}).$mount('#app')
