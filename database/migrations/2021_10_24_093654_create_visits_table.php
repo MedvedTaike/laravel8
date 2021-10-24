f392571799e82f05f9572c568463aa890e60ff26
Medved Taike <taike_dev@mail.ru>
2021-10-24 15:41:08 +0600

    Api and migration

--- database/migrations/2021_10_24_093654_create_visits_table.php
+++ database/migrations/2021_10_24_093654_create_visits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateVisitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id('link_id');
+            $table->index('link_id');
+            $table->foreign('link_id')->references('id')->on('short_links')->onDelete('cascade');
+            $table->ipAddress('ip_address');
+            $table->string('user_agent');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('visits');
     }
 }
